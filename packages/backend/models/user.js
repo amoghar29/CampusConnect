aa3add5832ca76dd917b806b489982ed698ee19a
amogha r <amoghar23@gmail.com>
2024-10-27 14:10:06 +0530

    added logo in db schema

--- packages/backend/models/user.js
+++ packages/backend/models/user.js
@@ -6,10 +6,10 @@ const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   clubName: { type: String, required: true },
-  // logo: {
-  //   type: String, // This will store the URL or path to the logo image
-  //   default: '' // You can set a default logo if needed
-  // },
+  logo: {
+    type: String, // This will store the URL or path to the logo image
+    default: '' // You can set a default logo if needed
+  },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
