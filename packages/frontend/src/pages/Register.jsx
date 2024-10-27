e28ed8926076b4a154f8411a51cd577a5c3c3951
amogha r <amoghar23@gmail.com>
2024-10-27 20:02:21 +0530

    added registration success/failure

--- packages/frontend/src/pages/Register.jsx
+++ packages/frontend/src/pages/Register.jsx
@@ -4,15 +4,18 @@ import axios from 'axios';
 import RegistrationSuccess from '../components/RegistrationSuccess';
 import RegistrationFailure from '../components/RegistrationFailure';
 import RegistrationForm from '../components/RegistrationForm';
+
 export default function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [clubName, setClubName] = useState('');
   const [registrationSuccess, setRegistrationSuccess] = useState(false);
   const [registrationError, setRegistrationError] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   async function register(e) {
     e.preventDefault();
+    setIsSubmitted(true);
 
     try {
       const response = await axios.post(
@@ -44,21 +47,27 @@ export default function Register() {
     setClubName('');
     setRegistrationSuccess(false);
     setRegistrationError(false);
+    setIsSubmitted(false);
   };
 
   return (
-    <div>
-      {registrationSuccess && <RegistrationSuccess />}
-      {registrationError && <RegistrationFailure onTryAgain={handleTryAgain} />}
-      <RegistrationForm
-        email={email}
-        password={password}
-        clubName={clubName}
-        setEmail={setEmail}
-        setPassword={setPassword}
-        setClubName={setClubName}
-        register={register}
-      />
+    <div className="min-h-screen bg-black text-white px-[10%]"> {/* Added 10% padding */}
+      {isSubmitted ? (
+        <>
+          {registrationSuccess && <RegistrationSuccess />}
+          {registrationError && <RegistrationFailure onTryAgain={handleTryAgain} />}
+        </>
+      ) : (
+        <RegistrationForm
+          email={email}
+          password={password}
+          clubName={clubName}
+          setEmail={setEmail}
+          setPassword={setPassword}
+          setClubName={setClubName}
+          register={register}
+        />
+      )}
     </div>
   );
 }
