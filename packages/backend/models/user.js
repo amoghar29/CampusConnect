4244d20a898a95ddf18b35aaea03445f8de255d2
amogha r <amoghar23@gmail.com>
2024-10-27 09:51:47 +0530

    added schema

--- packages/backend/models/user.js
+++ packages/backend/models/user.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
-const { Schema, model } = mongoose;
+const ObjectId = mongoose.ObjectId;
 
+
+const Schema = mongoose.Schema()
 const UserSchema = new Schema({
-  username: { type: String, required: true, unique: true },
+  email : { type: String, required: true, unique: true},  
   password: { type: String, required: true },
+  clubName :{ type : String , required : true},
+  logo: { 
+    type: String, // This will store the URL or path to the logo image
+    default: '' // You can set a default logo if needed
+  },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
 });
 
-const User = model('User', UserSchema);
+const userModel  = mongoose.model('User', UserSchema);
 
-module.exports = { User };
+module.exports = { userModel };
