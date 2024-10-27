3d44487afb89579b8c8b30e3508571bf584e003f
amogha r <amoghar23@gmail.com>
2024-10-27 17:19:33 +0530

    added new env variables and deployment address

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
+const PORT = process.env.PORT
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // Ensure this matches the port you are trying to access
+    port: PORT,
   },
 });
